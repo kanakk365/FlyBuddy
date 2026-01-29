817c55a79e3fccd98fd3849e3dad080727ea9834
Kanak <kanakkumarmahala000@gmail.com>
2026-01-29 22:57:19 +0530

    fix : errors

--- src/api/reports.js
+++ src/api/reports.js
@@ -1,34 +1,22 @@
 import axiosInstance from "./axiosInstance";
 
 export const getUserReports = async (period) => {
-  try {
-    const response = await axiosInstance.get(
-      `/admin/reports/user?period=${period}`,
-    );
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axiosInstance.get(
+    `/admin/reports/user?period=${period}`,
+  );
+  return response.data;
 };
 
 export const getBookingReports = async (startDate, endDate) => {
-  try {
-    const response = await axiosInstance.get(
-      `/admin/reports/booking?startDate=${startDate}&endDate=${endDate}`,
-    );
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axiosInstance.get(
+    `/admin/reports/booking?startDate=${startDate}&endDate=${endDate}`,
+  );
+  return response.data;
 };
 
 export const getRevenueReports = async (period) => {
-  try {
-    const response = await axiosInstance.get(
-      `/admin/reports/revenue?period=${period}`,
-    );
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axiosInstance.get(
+    `/admin/reports/revenue?period=${period}`,
+  );
+  return response.data;
 };
